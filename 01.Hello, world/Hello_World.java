78103290faf6e7a995d53896df3c6d8608d7aada
nongsang <shdtkd37@gmail.com>
2019-06-19 00:55:47 +0900

    6월 19일 Hello, world!

---
+++ 01.Hello, world/Hello_World.java
@@ -0,0 +1,9 @@
+package Java;   // 현재 코드가 존재하는 패키지명
+
+public class Hello_World    // 클래스의 이름, 소스코드의 이름과 같아야 한다.
+{
+    public static void main(String[] args)  // 메인 메서드
+    {
+        System.out.println("Hello, world!");    // 문자열 출력
+    }
+}
