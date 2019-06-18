87e09bef66acabfee83ebe77e96f36011accf1b8
nongsang <shdtkd37@gmail.com>
2019-06-19 02:53:39 +0900

    6월 19일 변수 이름

--- 02.Variable and Types/01.Variable_Name.java
+++ 02.Variable and Types/01.Variable_Name.java
@@ -14,6 +14,6 @@ public class Variable_Name
         int playerStrength = 6; // 변수 이름이 두 단어 이상으로 이루어져 있을경우
         int carBodyColor = 7;   // 첫 단어는 소문자, 다은 단어부터는 대문자로 시작한다. (관례)
 
-        //int int;  // 이미 예약되어 있는 명령어는 변수이름으로 사용할 수 없다.
+        int int;  // 이미 예약되어 있는 명령어는 변수이름으로 사용할 수 없다.
     }
 }
