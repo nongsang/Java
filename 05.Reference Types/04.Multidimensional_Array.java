be80e6225cd2c666d470df84b3c12f135244748e
nongsang <shdtkd37@gmail.com>
2019-06-24 14:48:30 +0900

    6월 24일 다차원 배열

---
+++ 05.Reference Types/04.Multidimensional_Array.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class Multidimensional_Array
+{
+    public static void main(String[] args)
+    {
+        int[][] intMat = new int[2][3]; // 2X3 2차원 배열 생성
+                                        // 모든 원소는 0으로 자동설정
+
+        int[][][] intVol = new int[3][3][3];    // 3X3X3 3차원 배열 생성
+                                                // 모든 원소는 0으로 자동설정
+    }
+}
