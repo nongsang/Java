2ebcc741e153e7a6782c19865f0052724539bff5
nongsang <shdtkd37@gmail.com>
2019-06-24 14:44:34 +0900

    6월 24일 조건문 if

---
+++ 04.Statements/01.Conditional_Statement_If.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Conditional_Statement_If
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+
+        if(i > 1)   // 조건에 맞으면 실행된다.
+        {
+            System.out.println("i는 1보다 크지 않다.");    // 조건에 맞지 않으므로 실행되지 않는다.
+        }
+
+        System.out.println("끝");
+    }
+}
