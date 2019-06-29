e44f750a97c425830a76a22240c0e19cb7e3a5e7
nongsang <shdtkd37@gmail.com>
2019-06-30 02:30:10 +0900

    6월 30일 인스턴스 멤버 클래스

---
+++ 09.Nested Class/01.Instance_Member_Class.java
@@ -0,0 +1,37 @@
+package Java;
+
+class Hard_Disk_Drive   // 하드디스크 클래스
+{
+    class Platter       // 인스턴스 멤버 클래스로 플래터 정의
+    {
+        Platter() {}    // 생성자 정의 가능
+
+        int saveData;   // 필드 정의 가능
+
+        static int page;    // static 필드 불가능
+
+        void Spin()     // 메서드 정의 가능
+        {
+            System.out.println("돈다");
+        }
+
+        static void Save()  // static 메서드 정의 불가능
+        {
+            System.out.println("저장한다.");
+        }
+    }
+}
+
+public class Instance_Member_Class
+{
+    public static void main(String[] args)
+    {
+        Hard_Disk_Drive HDD = new Hard_Disk_Drive();    // 하드디스크 생성
+
+        Hard_Disk_Drive.Platter platter = HDD.new Platter();    // 플래터는 하드디스크안에 있는 부품이다.
+                                                                // 따라서 이미 생성된 하드디스크에서 플래터를 생성한다.
+
+        platter.Spin();         // 플래터가 돌면서
+        platter.saveData = 30;  // 플래터에 데이터가 저장된다.
+    }
+}
