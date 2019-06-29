499e95193f6dadccf2fadb36fe3742c0e9703f84
nongsang <shdtkd37@gmail.com>
2019-06-30 04:31:53 +0900

    6월 30일 인스턴스 멤버 클래스

--- 09.Nested Class/01.Instance_Member_Class.java
+++ 09.Nested Class/01.Instance_Member_Class.java
@@ -1,24 +1,32 @@
 package Java;
 
-class Hard_Disk_Drive   // 하드디스크 클래스
+class A
 {
-    class Platter       // 인스턴스 멤버 클래스로 플래터 정의
+    A()     // A 생성자
     {
-        Platter() {}    // 생성자 정의 가능
+        System.out.println("A 생성");
+    }
 
-        private int saveData;   // 필드 정의 가능
-                                // 접근제어자 선언 가능
-
-        static int page;    // static 필드 불가능
-
-        void Spin()     // 메서드 정의 가능
+    class B     // 인스턴스 멤버 생성자
+    {
+        B()     // B 생성자
         {
-            System.out.println("돈다");
+            System.out.println("B 생성");
         }
 
-        static void Save()  // static 메서드 정의 불가능
+        int field1; // 인스턴스 필드 정의 가능
+                    // 접근제어자도 선언 가능
+
+        static int field2;  // static 필드 정의 불가능
+
+        void Method1()      // 메서드 정의 가능
         {
-            System.out.println("저장한다.");
+            System.out.println("Method1() 호출");
+        }
+
+        static void Method2()   // static 메서드 정의 불가능
+        {
+            System.out.println("Method2() 호출");
         }
     }
 }
@@ -27,13 +35,11 @@ public class Instance_Member_Class
 {
     public static void main(String[] args)
     {
-        Hard_Disk_Drive HDD = new Hard_Disk_Drive();    // 하드디스크 생성
+        A a = new A();      // A 생성
 
-        Hard_Disk_Drive.Platter platter = HDD.new Platter();    // 플래터는 하드디스크안에 있는 부품이다.
-                                                                // 따라서 이미 생성된 하드디스크에서 플래터를 생성한다.
+        A.B b = a.new B();  // A가 생성된 후에 B를 생성할 수 있다.
 
-        platter.Spin();         // 플래터가 돌면서
-        platter.saveData = 30;  // 플래터에 데이터가 저장된다.
-                                // 하지만 private이므로 직접접근 불가능
+        b.field1 = 3;   // B 인스턴스 필드 접근
+        b.Method1();    // B 인스턴스 메서드 호출
     }
 }
