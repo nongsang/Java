40e91d0d1dde7091d6a36037b9104910f93700aa
nongsang <shdtkd37@gmail.com>
2019-12-11 14:22:06 +0900

    12월 11일 FileInputStream

---
+++ 16.IO/11.FileInputStream_Path.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.io.*;   // 자바의 기본적인 데이터 입출력 API 제공
+
+public class FileInputStream_Path
+{
+    public static void main(String[] args) throws Exception // 입출력을 위해서 예외처리를 해줘야 한다.
+    {
+        // FileOutputStream은 바이트단위로 저장하는 클래스다.
+        // 동영상, 그림을 다룰 때 사용
+
+    }
+}
